80e9acde8de20a1caf9e0ff8d843e1d62d8cb270
b1gd3vd0g <devin@bigdevdog.com>
2024-06-14 11:46:35 -0700

    took out schema logic from models module.

--- backend/models.js
+++ backend/models.js
@@ -1,36 +1,16 @@
-const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema(
-  {
-    email: {
-      type: String,
-      required: true
-    },
-    username: {
-      type: String,
-      required: true
-    },
-    password: {
-      type: String,
-      required: true
-    },
-    salt: {
-      type: String,
-      required: true
-    },
-    projects: {
-      type: [String],
-      required: true
-    }
-  },
-  {
-    collection: 'users'
-  }
-);
-
-const connectionString = (db) => {
+/** A function to connect to a database within the scrumblebee-cluster. */
+const mongoUri = (db) => {
   const credentials = require('./config.json').mongodb;
   return `mongodb+srv://${credentials.username}:${credentials.password}@scrumblebee-cluster.zjmfqxn.mongodb.net/${db}?retryWrites=true&w=majority&appName=scrumblebee-cluster`;
 };
 
-const User = new mongoose.Model('User', UserSchema);
+const mongoose = require('mongoose');
+const { UserSchema } = require('./schemas');
+
+const dbConnection = mongoose.createConnection(mongoUri('scrumblebee'));
+
+const User = dbConnection.model('User', UserSchema);
+
+module.exports = {
+  User
+};
