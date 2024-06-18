ec6688cc48709c107b533e79fd3a44c2ccec2952
b1gd3vd0g <devin@bigdevdog.com>
2024-06-18 12:59:26 -0700

    CREATE operation is functional.

--- user-ms/base_router.js
+++ user-ms/base_router.js
@@ -1,20 +1,37 @@
 const router = require('express').Router();
 
 const authRouter = require('./auth_router');
+const { createUser } = require('./user_funcs');
 
 router.use('/auth', authRouter);
-
 /*
   // get users (with optional filters)
   router.get('/', async (req, res) => {
     
   });
-
-  // create a new user
-  router.post('/', async (req, res) => {
-    
-  });
-
+*/
+// create a new user
+router.post('/', async (req, res) => {
+  const body = req.body;
+  const un = body.username;
+  const pw = body.password;
+  const em = body.email;
+  if (
+    un &&
+    typeof un === 'string' &&
+    pw &&
+    typeof pw === 'string' &&
+    em &&
+    typeof em === 'string'
+  ) {
+    const creation = await createUser(un, em, pw);
+    const code = creation.code;
+    delete creation.code;
+    return res.status(code).json(creation);
+  }
+  return res.status(400).json({ success: false, error: 'missing fields' });
+});
+/*
   // delete a user
   router.delete('/', async (req, res) => {
     
