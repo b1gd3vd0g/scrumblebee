d36d5bd344e5fa1d608133b0e27c3bde9bbf66fb
b1gd3vd0g <devin@bigdevdog.com>
2024-06-24 19:56:32 -0700

    empty base router
    what's important is how it uses the auth router at the path '/auth'

--- user-ms/base_router.js
+++ user-ms/base_router.js
@@ -4,43 +4,13 @@ const authRouter = require('./auth_router');
 const { createUser } = require('./user_funcs');
 
 router.use('/auth', authRouter);
-/*
-  // get users (with optional filters)
-  router.get('/', async (req, res) => {
-    
-  });
-*/
+// get users (with optional filters)
+router.get('/', async (req, res) => {});
 // create a new user
-router.post('/', async (req, res) => {
-  const body = req.body;
-  const un = body.username;
-  const pw = body.password;
-  const em = body.email;
-  if (
-    un &&
-    typeof un === 'string' &&
-    pw &&
-    typeof pw === 'string' &&
-    em &&
-    typeof em === 'string'
-  ) {
-    const creation = await createUser(un, em, pw);
-    const code = creation.code;
-    delete creation.code;
-    return res.status(code).json(creation);
-  }
-  return res.status(400).json({ success: false, error: 'missing fields' });
-});
-/*
-  // delete a user
-  router.delete('/', async (req, res) => {
-    
-  });
-
-  // update a user
-  router.post('/', async (req, res) => {
-    
-  });
-*/
+router.post('/', async (req, res) => {});
+// delete a user
+router.delete('/', async (req, res) => {});
+// update a user
+router.put('/', async (req, res) => {});
 
 module.exports = router;
