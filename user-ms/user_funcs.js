0ebd9fcb845600e6beba2143ff86b42f683084e8
b1gd3vd0g <devin@bigdevdog.com>
2024-06-24 19:53:06 -0700

    exports functions to create an account and verify a password/un combo

--- user-ms/user_funcs.js
+++ user-ms/user_funcs.js
@@ -1,10 +1,57 @@
 const { generateSalt, hash } = require('./hashing');
 const { User } = require('./models');
 const {
+  prettify,
+  validateAll,
   validateUsername,
   validateEmail,
   validatePassword
 } = require('./validators');
+
+const sse = (err) => {
+  return {
+    success: false,
+    message: 'server-side error',
+    code: 500,
+    error: err
+  };
+};
+
+const authenticateUserLogin = async (usernameOrEmail, password) => {
+  const un = prettify(usernameOrEmail);
+  const pw = prettify(password);
+  try {
+    const existingAcct =
+      (await User.findOne({ username: un })) ||
+      (await User.findOne({ email: un }));
+
+    if (existingAcct) {
+      const salt = existingAcct.salt;
+      const hashedPw = hash(pw, salt);
+      if (hashedPw === existingAcct.password) {
+        const jwt = require('jsonwebtoken');
+        const token = jwt.sign(
+          {
+            uid: existingAcct._id,
+            un: existingAcct.un
+          },
+          require('./config.json').jwtSecret,
+          {
+            expiresIn: 3_600 // 1 hr = 3_600 sec
+          }
+        );
+
+        jwt.decode();
+        return { success: true, code: 200, token: token };
+      }
+    }
+    // 401 exit: input is valid, authentication failed. incorrect login info.
+    return { success: false, code: 401, message: 'auth failed' };
+  } catch (e) {
+    // 500 exit: server-side error
+    return sse(e);
+  }
+};
 
 const createUser = async (username, email, password) => {
   // 0. Trim input
@@ -61,4 +108,4 @@ const createUser = async (username, email, password) => {
   }
 };
 
-module.exports = { createUser };
+module.exports = { createUser, authenticateUserLogin };
