fdb304b1266ed9e8cf1c6d38fa046432dd7a2be4
b1gd3vd0g <devin@bigdevdog.com>
2024-06-24 20:00:50 -0700

    define the requests that you will be calling (will save lots of clutter in things like button onClicks

--- frontend/src/funcs/requests.js
+++ frontend/src/funcs/requests.js
@@ -1,30 +1,11 @@
+import { User } from '../../../user-ms/models';
+import {
+  validateEmail,
+  validatePassword,
+  validateUsername
+} from './validators';
+
 const requestCreateAcct = async (username, email, password) => {
-  // 0: Define validation functions
-  const validateUsername = (un) => {
-    return (
-      typeof un === String &&
-      // Good length
-      un.length > 5 &&
-      un.length <= 15 &&
-      // No illegal characters
-      /^[A-Za-z0-9._-]+$/.test(un)
-    );
-  };
-  const validateEmail = (em) => {
-    return typeof em === String && /^.+@.+\..+$/.test(em);
-  };
-  const validatePassword = (pw) => {
-    return (
-      typeof pw === String &&
-      // contains uppercase, lowercase, number, symbol
-      /[A-Z]/.test(pw) &&
-      /[a-z]/.test(pw) &&
-      /[0-9]/.test(pw) &&
-      /[!@#$%^&*?/+=_-]/.test(pw) &&
-      // no unexpected characters
-      /^[A-Za-z0-9!@#$%^&*?/+=_-]$+/.test(pw)
-    );
-  };
   // 1. Trim and validate input. Store invalid fields in err. Fail if invalid.
   const un = username.trim();
   const em = email.trim();
@@ -36,11 +17,33 @@ const requestCreateAcct = async (username, email, password) => {
   if (err.length)
     return { success: false, message: 'bad request', fields: err };
   // 2. Make the request
-  const result = await fetch('localhost:4043/auth', {
-    username: un,
-    email: em,
-    password: pw
+  const result = await fetch('http://localhost:4043/', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({
+      username: un,
+      email: em,
+      password: pw
+    })
   });
   const creation = await result.json();
   console.log(creation);
+  return creation;
 };
+
+const authenticateCredentials = async (usernameOrEmail, password) => {
+  const un = usernameOrEmail.trim();
+  const pw = password.trim();
+  const result = await fetch('http://localhost:4043/auth', {
+    headers: { 'Content-Type': 'application/json' },
+    method: 'POST',
+    body: JSON.stringify({
+      username: un,
+      password: pw
+    })
+  });
+  const signin = await result.json();
+  return signin;
+};
+
+export { requestCreateAcct };
