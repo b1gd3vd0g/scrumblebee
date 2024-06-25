1479446514d075faf8559a1b77e7d0c1a1456659
b1gd3vd0g <devin@bigdevdog.com>
2024-06-24 20:02:24 -0700

    add create account component which reads input and requests to create an account accordingly

--- frontend/src/components/create_acct.js
+++ frontend/src/components/create_acct.js
@@ -1,35 +1,123 @@
+import '../css/create_acct.css';
+import scrumblebee from '../img/scrumblebee_icon_trans.png';
+import {
+  validateEmail,
+  validatePassword,
+  validateUsername
+} from '../funcs/validators';
+import { createContext, useContext, useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { requestCreateAcct } from '../funcs/requests';
+
+const CreationContext = createContext();
+
 const CreateAcct = () => {
+  const [creation, setCreation] = useState({});
+  console.log(creation);
+
+  useEffect(() => {
+    if (creation.send) {
+      const un = creation.username;
+      const pw = creation.password;
+      const em = creation.email;
+      (async () => {
+        const result = await requestCreateAcct(un, em, pw);
+        if (!result.success)
+          setCreation({
+            ...creation,
+            send: false,
+            feedback: result.message
+          });
+        else
+          setCreation({
+            ...creation,
+            success: result.success,
+            feedback: result.message
+          });
+      })();
+    }
+  }, [creation.send]);
+
+  const initialCreationContext = { creation, setCreation };
   return (
     <div className='CreateAcct'>
-      <h1>Create a new account</h1>
-      <FormGroup id='username' label='Username' />
-      <FormGroup id='password' label='Password' type='password' />
-      <FormGroup id='email' label='Email address' type='email' />
-      <CreateAcctBtn />
+      <CreationContext.Provider value={initialCreationContext}>
+        <img src={scrumblebee} />
+        <h1>Create a new account</h1>
+        <FormGroup
+          id='username'
+          label='Username'
+          validator={validateUsername}
+        />
+        <FormGroup
+          id='password'
+          label='Password'
+          type='password'
+          validator={validatePassword}
+        />
+        <FormGroup
+          id='email'
+          label='Email address'
+          type='email'
+          validator={validateEmail}
+        />
+        <CreateAcctBtn />
+        <span>{creation.feedback}</span>
+      </CreationContext.Provider>
     </div>
   );
 };
 
-const FormGroup = ({ id, label, type = 'text' }) => {
+const FormGroup = ({ id, label, type = 'text', validator = (i) => true }) => {
+  const { creation, setCreation } = useContext(CreationContext);
+
+  const validate = () => {
+    const element = document.querySelector(`#${id}`);
+    const input = element.value.trim();
+    const valid = validator(input);
+    if (valid) {
+      element.classList.remove('invalid');
+      setCreation({
+        ...creation,
+        [id]: input
+      });
+    } else {
+      element.classList.add('invalid');
+      setCreation({
+        ...creation,
+        [id]: undefined
+      });
+    }
+  };
+
   return (
     <div className='FormGroup'>
       <label>{label}</label>
-      <input id={id} type={type} />
+      <input id={id} type={type} onBlur={validate} />
     </div>
   );
 };
 
 const CreateAcctBtn = () => {
-  const createAcct = async () => {};
+  const { creation, setCreation } = useContext(CreationContext);
+  const disabled =
+    !(creation.username && creation.password && creation.email) ||
+    creation.send;
+
+  const attemptCreation = async (evt) => {
+    evt.preventDefault();
+    setCreation({ ...creation, send: true });
+  };
 
   return (
-    <button
-      onClick={(e) => {
-        e.preventDefault();
-        createAcct();
-      }}>
-      Create Account
-    </button>
+    <>
+      <button disabled={disabled} onClick={attemptCreation}>
+        Create Account
+      </button>
+      <Link to='/'>
+        <button>Back to welcome</button>
+      </Link>
+    </>
   );
 };
 
