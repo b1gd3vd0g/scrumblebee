6053360e7c42bd387d75723c0f4f6c3cb9d3ca2f
b1gd3vd0g <devin@bigdevdog.com>
2024-06-25 18:07:50 -0700

    fixed a 'get' method having a body, use a query now to ensure the fields match.

--- user-ms/auth_router.js
+++ user-ms/auth_router.js
@@ -3,11 +3,14 @@ const router = require('express').Router();
 const { User } = require('./models');
 const { authenticateUserLogin } = require('./user_funcs');
 const { hash } = require('./hashing');
+const { prettify } = require('./validators');
 
 const DEFAULT_HEADERS = {
   'Content-Type': 'application/json',
   authorization: `Bearer ${process.env.AUTH_API_KEY}`
 };
+
+router.use('/login', require('./login_router'));
 
 // get the authentication status of a token you have.
 router.get('/', async (req, res) => {
@@ -18,21 +21,32 @@ router.get('/', async (req, res) => {
       message: 'session token does not exist'
     });
   }
+
+  const queryString = req.query
+    ? ((query) => {
+        let str = '';
+        const keys = Object.keys(query);
+        for (let i = 0; i < keys.length; i++) {
+          if (i === 0) str += '?';
+          str += keys[i];
+          str += '=';
+          str += query[keys[i]];
+          if (i + 1 < keys.length) str += '&';
+        }
+        return str;
+      })(req.query)
+    : '';
   // ensure that the token is valid.
-  const validationResponse = await fetch('http://localhost:4042', {
-    headers: {
-      ...DEFAULT_HEADERS,
-      token: `Bearer ${token}`
-    },
-    method: 'GET',
-    requestBody: JSON.stringify({
-      // make sure that the jwt is FOR the account we're signed into.
-      expected: {
-        uid: req.session.uid,
-        username: req.session.username
-      }
-    })
-  });
+  const validationResponse = await fetch(
+    `http://localhost:4042${queryString}`,
+    {
+      headers: {
+        ...DEFAULT_HEADERS,
+        token: `Bearer ${token}`
+      },
+      method: 'GET'
+    }
+  );
   const validation = await validationResponse.json();
   if (validation.success) {
     return res.status(200).json(validation);
@@ -42,60 +56,43 @@ router.get('/', async (req, res) => {
 
 // Start a session and get a token verifying that you are logged in.
 router.post('/', async (req, res) => {
-  const un = req.body.username;
-  const pw = req.body.password;
+  const un = prettify(req.body.username) ?? '';
+  const pw = prettify(req.body.password) ?? '';
   if (!un || !pw || typeof un !== 'string' || typeof pw !== 'string') {
     return res.status(400).json({
       success: false,
       message: 'bad request! username or password not provided!'
     });
   }
-  // find the account
-  const acct =
-    (await User.findOne({ username: un })) ||
-    (await User.findOne({ email: un }));
-  const badLogin = {
-    success: false,
-    message: 'input did not match our records!'
-  };
-  if (!acct) return res.status(401).json(badLogin);
-  // account exists. verify password.
-  const hashedPw = hash(pw, acct.salt);
-  if (hashedPw !== acct.password) return res.status(401).json(badLogin);
-  // passwords match!
-  // call authentication API
-  try {
-    // request a token
-    const response = await fetch('http://localhost:4042', {
-      headers: DEFAULT_HEADERS,
-      method: 'POST',
-      body: JSON.stringify({
-        payload: {
-          uid: acct._id,
-          username: acct.username
-        }
-      })
-    });
-    const outcome = await response.json();
-    if (outcome.success) {
-      // set the session info
-      req.session.token = outcome.token;
-      req.session.uid = acct._id;
-      req.session.username = acct.username;
-      return res.status(200).json({
-        success: true,
-        message: `session started for ${acct.username}`,
-        uid: acct._id
-      });
-    }
-    return res.status(500).json(outcome);
-  } catch (e) {
-    return res.status(500).json({
-      success: false,
-      message: 'error contacting auth api',
-      error: e
-    });
+
+  //console.log(un, pw);
+
+  // You have the username and password.
+  // Now, this server (the user microservice) needs to send it to the auth
+  // microservice, so that it can test with our private '/verify' function and
+  // then provide us with a token!
+  const response = await fetch('http://localhost:4042', {
+    headers: {
+      authorization: `Bearer ${process.env.AUTH_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      username: un,
+      password: pw
+    })
+  });
+  const obj = await response.json();
+  if (!obj.success) {
+    return res.status(response.status).json(obj);
   }
+  // we logged in successfully! Now we need to set the session info!
+  req.session.token = obj.token;
+  req.session.uid = obj.uid;
+  req.session.username = obj.username;
+  return res
+    .status(200)
+    .json({ success: true, username: obj.username, uid: obj.uid });
 });
 
 router.delete('/', async (req, res) => {
