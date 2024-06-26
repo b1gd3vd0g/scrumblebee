95dcc33ade1cb5c7fec422cee54219cedf5f97c1
b1gd3vd0g <devin@bigdevdog.com>
2024-06-25 18:01:45 -0700

    updated for changes to the .env file

--- auth_ms/functions.js
+++ auth_ms/functions.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const isApiKey = (token) => {
   try {
-    const verification = jwt.verify(token, process.env.API_KEY_SECRET);
+    const verification = jwt.verify(token, process.env.AUTH_API_SECRET);
     return (
       // ensure equality.
-      verification.a === process.env.API_KEY_A &&
-      verification.b === process.env.API_KEY_B &&
-      verification.c === process.env.API_KEY_C &&
-      verification.d === process.env.API_KEY_D
+      verification.a === process.env.AUTH_API_A &&
+      verification.b === process.env.AUTH_API_B &&
+      verification.c === process.env.AUTH_API_C &&
+      verification.d === process.env.AUTH_API_D
     );
   } catch (e) {
     // could not decode the token
