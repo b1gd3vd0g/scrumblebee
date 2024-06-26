9cc52b28494e0d623ccc8b29a6f10465ee4db96c
b1gd3vd0g <devin@bigdevdog.com>
2024-06-25 18:02:19 -0700

    the auth-ms now relies upon the user-ms to authenticate a username and password.

--- auth_ms/base_router.js
+++ auth_ms/base_router.js
@@ -3,7 +3,14 @@ const { isApiKey, meetsExpectations } = require('./functions');
 
 const router = require('express').Router();
 
-/** Checks the authorization header  */
+/**
+ * GET: [server]:4042?username=b1gd3vd0g
+ *
+ * This function is only accessible to other scrumblebee microservices
+ * (requires API key).
+ *
+ *
+ */
 router.get('/', (req, res) => {
   // 1. verify api key.
   // The api key should be found in the 'authorization' header.
@@ -11,7 +18,7 @@ router.get('/', (req, res) => {
   if (typeof authHeader !== 'string') {
     return res.status(403).json({
       pass: false,
-      message: 'authorization header not found.'
+      message: 'api key missing for the auth microservice'
     });
   }
   // extract actual token
@@ -20,13 +27,13 @@ router.get('/', (req, res) => {
   if (!validApiKey) {
     return res.status(403).json({
       pass: false,
-      message: 'invalid api key.'
+      message: 'invalid api key for the auth microservice'
     });
   }
   // The api key is valid!
 
   // 2. check out value token
-  const secret = process.env.SCRUMBLEBEE_SECRET;
+  const secret = process.env.AUTH_TOKEN_SECRET;
   const tokenHeader = req.headers['token'];
   if (typeof tokenHeader !== 'string') {
     return res.status(400).json({
@@ -38,10 +45,11 @@ router.get('/', (req, res) => {
   try {
     // get the payload of the token
     const verification = jwt.verify(token, secret);
-    if (!req.body.expected) {
+    console.log(req.query);
+    if (!req.query) {
       return res.status(200).json({ pass: true });
     }
-    const expectations = req.body.expected;
+    const expectations = req.query ?? {};
     const matches = meetsExpectations(verification, expectations);
     if (matches.success) {
       return res.status(200).json({
@@ -69,8 +77,8 @@ router.get('/', (req, res) => {
 });
 
 /** Returns a token valid for 60 minutes */
-router.post('/', (req, res) => {
-  // 1. verify api key.
+router.post('/', async (req, res) => {
+  // 0. verify api key.
   // The api key should be found in the 'authorization' header.
   const authHeader = req.headers['authorization'];
   if (typeof authHeader !== 'string') {
@@ -85,20 +93,37 @@ router.post('/', (req, res) => {
   if (!validApiKey) {
     return res.status(403).json({
       pass: false,
-      message: 'invalid api key.'
+      message: 'invalid api key. auth-ms/'
     });
   }
   // The api key is valid!
 
-  // 2. create a new token.
-  if (!req.body.payload) {
+  // 1. test the username and password for correctness.
+  if (!req.body.username || !req.body.password) {
     return res.status(400).json({
       success: false,
-      message: 'no payload was included!'
+      message: 'either username or password is missing.'
     });
   }
-  const payload = req.body.payload;
-  const secret = process.env.SCRUMBLEBEE_SECRET;
+  const response = await fetch('http://localhost:4044/auth/login', {
+    headers: {
+      authorization: `Bearer ${process.env.LOGIN_API_KEY}`,
+      'Content-Type': 'application/json'
+    },
+    method: 'POST',
+    body: JSON.stringify({
+      username: req.body.username,
+      password: req.body.password
+    })
+  });
+  const correctness = await response.json();
+  console.log(correctness);
+  if (!correctness.success) {
+    return res.status(401).json({ success: false, message: 'login failed' });
+  }
+  // 2. create a new token.
+  const payload = { username: correctness.username, uid: correctness.uid };
+  const secret = process.env.AUTH_TOKEN_SECRET;
   const token = jwt.sign(payload, secret, { expiresIn: 3_600 });
   return res.status(200).json({ success: true, token: token });
 });
