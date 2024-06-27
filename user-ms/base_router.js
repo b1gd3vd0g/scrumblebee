7a5f45e6499a7f64efe6ba67185a3dbec7ca4df6
b1gd3vd0g <devin@bigdevdog.com>
2024-06-26 22:40:25 -0700

    removed placeholder code (for now)

--- user-ms/base_router.js
+++ user-ms/base_router.js
@@ -1,16 +1,5 @@
 const router = require('express').Router();
 
-const authRouter = require('./auth_router');
-const { createUser } = require('./user_funcs');
-
-router.use('/auth', authRouter);
-// get users (with optional filters)
-router.get('/', async (req, res) => {});
-// create a new user
-router.post('/', async (req, res) => {});
-// delete a user
-router.delete('/', async (req, res) => {});
-// update a user
-router.put('/', async (req, res) => {});
+router.use('/session', require('./session_router'));
 
 module.exports = router;
