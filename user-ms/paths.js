87038b1c0e31466c381f527286f523ff54b39dcb
b1gd3vd0g <devin@bigdevdog.com>
2024-07-02 17:46:07 -0700

    added jsdoc

--- user-ms/paths.js
+++ user-ms/paths.js
@@ -4,7 +4,10 @@ const authPort = process.env.AUTH_PORT;
 const projPort = process.env.PROJ_PORT;
 
 module.exports = {
+  /** The path to the auth-ms. */
   AUTH: `${host}:${authPort}`,
+  /** The path to the user-ms. */
   USER: `${host}:${userPort}`,
+  /** The path to the project-ms. */
   PROJ: `${host}:${projPort}`
 };
