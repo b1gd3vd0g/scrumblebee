6b061b8d4bcb91dc253b9400c72cf3f90346443d
b1gd3vd0g <devin@bigdevdog.com>
2024-07-04 15:57:10 -0700

    adjusted how im working the user functions

--- user-ms/mongoose/user.js
+++ user-ms/mongoose/user.js
@@ -1,36 +1,66 @@
 const { generateSalt, hash } = require('./hashing');
 const User = require('./model');
+const { validateAll } = require('./validators');
 
 User.createAccount = async (username, password, email) => {
   try {
+    // validate the input.
+    const validation = validateAll(username, password, email);
+    if (!validation.valid) {
+      // there are invalid fields.
+      return { code: 400, probs: validation.probs };
+    }
+    // all fields contain valid input.
     const salt = generateSalt();
-    const hashed = hash(password, salt);
-    const user = new User({
-      username: username,
-      password: hashed,
-      email: email,
+    const hashedPw = hash(validation.values.password, salt);
+    const newUser = new User({
+      username: validation.values.username,
+      email: validation.values.email,
+      password: hashedPw,
       salt: salt
     });
-    return await user.save();
-  } catch (err) {
-    return false;
+    await newUser.save();
+    // successfully created account!
+    return { code: 201 };
+  } catch (e) {
+    // server-side error
+    return { code: 500, error: e };
   }
 };
 
 User.verifyCredentials = async (username, password) => {
-  const noMatch = { message: 'invalid credentials.' };
+  const noMatch = { code: 404 };
+  if (
+    !username ||
+    !password ||
+    typeof username !== 'string' ||
+    typeof password !== 'string'
+  ) {
+    return { code: 400 };
+  }
   try {
     const account =
       (await User.findOne({ username: username })) ||
       (await User.findOne({ email: username }));
     if (!account) return noMatch;
     const hashedPassword = hash(password, account.salt);
-    if (hashedPassword === account.password)
-      return { success: true, uid: account._id, username: account.username };
-    return noMatch;
+    if (hashedPassword === account.password) {
+      // credentials are valid
+      // return uid and username so auth-ms can create a token.
+      return {
+        code: 200,
+        account: {
+          uid: account._id,
+          username: account.username
+        }
+      };
+    } else {
+      return noMatch;
+    }
   } catch (e) {
     console.error(e);
-    return { message: 'error', error: e };
+    // server side error.
+    return { code: 500, error: e };
   }
 };
 
