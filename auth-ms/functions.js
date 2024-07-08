1f0f1f18a6fa7772303118e63e1000dc8565172f
b1gd3vd0g <devin@bigdevdog.com>
2024-07-07 19:49:44 -0700

    changed variable name

--- auth-ms/functions.js
+++ auth-ms/functions.js
@@ -2,13 +2,13 @@ const jwt = require('jsonwebtoken');
 
 const isApiKey = (token) => {
   try {
-    const verification = jwt.verify(token, process.env.AUTH_API_SECRET);
+    const payload = jwt.verify(token, process.env.AUTH_API_SECRET);
     return (
       // ensure equality.
-      verification.a === process.env.AUTH_API_A &&
-      verification.b === process.env.AUTH_API_B &&
-      verification.c === process.env.AUTH_API_C &&
-      verification.d === process.env.AUTH_API_D
+      payload.a === process.env.AUTH_API_A &&
+      payload.b === process.env.AUTH_API_B &&
+      payload.c === process.env.AUTH_API_C &&
+      payload.d === process.env.AUTH_API_D
     );
   } catch (e) {
     // could not decode the token
